78d2a77c34574f55bcd3916a1f5dedda6f1d42b7
Ben Chuan <benchuan0614@gmail.com>
2022-01-16 11:02:23 +0800

    將store移出pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import "../styles/sliderStyles.scss";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
-import { persistor } from "./store";
-import store from "./store";
+import { persistor } from "../store";
+import store from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
